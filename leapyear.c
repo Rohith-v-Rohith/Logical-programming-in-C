9d78f6d747b571e80c9869abcb747044dbf81403
Rohith V <rohirohithv948@gmail.com>
2026-03-17 20:21:14 +0530

    find the Leap years

---
+++ leapyear.c
@@ -0,0 +1,15 @@
+/*write  a program to print LEAP YEAR ? */
+
+#include<stdio.h>
+int main (){
+    int n;
+    printf("enter the year : ");
+    scanf("%d",&n);
+
+        if((n % 400 == 0 )|| n % 4 == 0 && n % 100 != 0 ){
+            printf("%d is a leap year",n);
+        }else{
+            printf("%d is not a leap year",n);
+        }
+return 0;
+}
